8d2cd38a34278d4e0b0330b4756b3838e80f71ed
phoenix010 <tyagiaditya62@gmail.com>
2020-11-27 23:01:41 -0600

    completed sign-up and login page

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping()
+    public String loginView() {
+        return "login";
+    }
+
+    @PostMapping
+    public String loginUser(){
+        //check the username and password submitted in the form with the database.
+        // If there are no errors go to home page otherwise display errors
+        return"login";
+    }
+}
+
